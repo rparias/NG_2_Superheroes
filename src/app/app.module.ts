a1dbe227c82b26c55122ba5039e396b3493858b4
ronald.arias <ronald.arias@udla.edu.ec>
2018-05-20 09:59:27 -0500

    Crear navbar y agregar favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
