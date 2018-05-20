7596b25b4b94a2438251ef1e1838db6b773e4f3a
ronald.arias <ronald.arias@udla.edu.ec>
2018-05-20 10:12:11 -0500

    Rutas en Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
