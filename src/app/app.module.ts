6a52d49da01aacafef41d3f4687da7ffb21b32e8
ronald.arias <ronald.arias@udla.edu.ec>
2018-05-20 12:41:05 -0500

    Crear un servicio de heroes llamado HeroesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
 
 //Servicios
-
+import { HeroesService } from './servicios/heroes.service';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -26,7 +26,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
 
+  //le paso al constructor un argumento llamado _heroesService de tipo HeroesService
+  //con esto el servicio queda listo para usar
+  constructor( private _heroesService:HeroesService ) {
+
+  }
+
+  //ngOnInit es para cuando ya esta toda la pagina renderizada
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
